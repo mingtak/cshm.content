62598677c6d5feda338783ced9eb46706d6d9188
Andy Fang <andy@mingtak.com>
2018-05-21 04:01:48 +0000

    add fields for Course content type

--- src/cshm/content/content/course.py
+++ src/cshm/content/content/course.py
@@ -6,6 +6,8 @@ from plone.namedfile import field as namedfile
 from plone.supermodel import model
 from plone.supermodel.directives import fieldset
 from z3c.form.browser.radio import RadioFieldWidget
+from z3c.relationfield.schema import RelationList, RelationChoice
+from plone.app.vocabularies.catalog import CatalogSource
 from zope import schema
 from zope.interface import implementer
 from cshm.content import _
@@ -14,6 +16,89 @@ from cshm.content import _
 class ICourse(model.Schema):
     """ Marker interface and Dexterity Python Schema for Course
     """
+
+    # fieldset('CourseDetail', fields=[])
+    reTrainingBaseOn = schema.TextLine(
+        title=_(u'Retraining Base On'),
+        required=False,
+    )
+
+    reTrainingCourse = RelationChoice(
+        title=_(u"Retrainging Course"),
+        required=False,
+        source=CatalogSource(Type='Course')
+    )
+
+    handbookTitle = schema.TextLine(
+        title=_(u'Handbook Title'),
+        required=True,
+    )
+
+    courseHours = schema.Int(
+        title=_(u"Course Hours"),
+        default=0,
+        description=_(u"If 0, asking for phone"),
+        required=True,
+    )
+
+    courseFee = schema.Int(
+        title=_(u"Course Fee"),
+        default=0,
+        description=_(u"If 0, asking for phone"),
+        required=True,
+    )
+
+    courseRequired = RichText(
+        title=_(u"Course Required"),
+        required=False,
+    )
+
+    trainee = RichText(
+        title=_(u"Trainee"),
+        required=False,
+    )
+
+    courseScope = schema.TextLine(
+        title=_(u"Course Scope"),
+        required=False,
+    )
+
+    courseEffective = schema.TextLine(
+        title=_(u"Course Effective"),
+        required=False,
+    )
+
+    introduction = RichText(
+        title=_(u"Introduction"),
+        required=False,
+    )
+
+    otherNote = RichText(
+        title=_(u"Other Note"),
+        required=False,
+    )
+
+    certificateCode = schema.TextLine(
+        title=_(u"Certificate Code"),
+        required=False,
+    )
+
+    sampleTitle = schema.TextLine(
+        title=_(u"Sample Title"),
+        required=False,
+    )
+
+    reTrainingYears = schema.Int(
+        title=_(u"Retraining Years"),
+        default=0,
+        description=_(u"if 0, no need retraining."),
+        required=True,
+    )
+
+    licenseType = schema.TextLine(
+        title=_(u"License Type"),
+        required=True,
+    )
 
     # directives.widget(level=RadioFieldWidget)
     # level = schema.Choice(
